9638c9d0d8003ce8250e54ca0036acefb1e08a25
Xing <xingmarc@yeah.net>
2018-06-18 23:11:10 -0700

    ...

---
+++ problems/src/lib.rs
@@ -0,0 +1,46 @@
+#[cfg(test)]
+mod tests {
+    use std::mem;
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+
+
+    #[test]
+    fn t1() {
+        assert_eq!(second_largest(vec![1]), None);
+        assert_eq!(second_largest(vec![1,2]), Some(1));
+        assert_eq!(second_largest(vec![3,1,2]), Some(2));
+        assert_eq!(second_largest(vec![3,1,2,2,2]), Some(2));
+        assert_eq!(second_largest(vec![3,1,2,100,99]), Some(99));
+        assert_eq!(second_largest(vec![3,1,2,1,1,1]), Some(2));
+        // assert_eq!(second_largest(vec![3,3,2,3,1,1]), Some(2));
+    }
+
+    fn second_largest(a: Vec<u8>) -> Option<u8> {
+        if a.len() < 2 {
+            return None;
+        }
+
+        let mut l = a[0];
+        let mut s = a[1];
+
+        if l < s {
+            mem::swap(&mut s, &mut l);
+        }
+
+        for i in a {
+            if i > l {
+                let m = l;
+                l = i;
+                s = m;
+            } else if i < l && i > s {
+                s = i;
+            }
+        }
+
+        Some(s)
+    }
+
+}
