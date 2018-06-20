a01c3ac44879ecf4306401c1e057e966f48ac257
Xing <xingmarc@yeah.net>
2018-06-19 23:14:32 -0700

    ...

--- problems/src/lib.rs
+++ problems/src/lib.rs
@@ -18,6 +18,17 @@ mod tests {
         // assert_eq!(second_largest(vec![3,3,2,3,1,1]), Some(2));
     }
 
+    #[test]
+    fn t2() {
+        assert_eq!(second_largest_2(vec![1]), None);
+        assert_eq!(second_largest_2(vec![1,2]), Some(1));
+        assert_eq!(second_largest_2(vec![3,1,2]), Some(2));
+        assert_eq!(second_largest_2(vec![3,1,2,2,2]), Some(2));
+        assert_eq!(second_largest_2(vec![3,1,2,100,99]), Some(99));
+        assert_eq!(second_largest_2(vec![3,1,2,1,1,1]), Some(2));
+        assert_eq!(second_largest_2(vec![3,3,2,3,1,1]), Some(2));
+    }
+
     fn second_largest(a: Vec<u8>) -> Option<u8> {
         if a.len() < 2 {
             return None;
@@ -43,4 +54,30 @@ mod tests {
         Some(s)
     }
 
+    fn second_largest_2(a: Vec<u8>) -> Option<u8> {
+        if a.len() < 2 {
+            return None;
+        }
+
+        let mut l = a[0];
+        for i in &a {
+            if i > &l {
+                l = *i;
+            }
+        }
+
+        let mut s = 0;
+
+        for i in &a {
+            if i == &l {
+                continue;
+            }
+
+            if i > &s {
+                s = *i;
+            }
+        }
+
+        Some(s)
+    }
 }
