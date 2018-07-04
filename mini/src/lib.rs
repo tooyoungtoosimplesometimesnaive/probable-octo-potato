93f3276b8a42dbf3b9f19f63884e928c1c32a88c
Xing <xingmarc@yeah.net>
2018-07-03 23:14:38 -0700

    ...

--- mini/src/lib.rs
+++ mini/src/lib.rs
@@ -1,7 +1,13 @@
+mod mini;
+
 #[cfg(test)]
 mod tests {
+    use mini;
+
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
+        mini::work();
     }
 }
+
---
+++ mini/src/mini.rs
@@ -0,0 +1,3 @@
+    pub fn work() {
+        println!("it works");
+    }
