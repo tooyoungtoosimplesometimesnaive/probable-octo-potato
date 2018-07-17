5382dbb4f8216f30cd9bc8c72917af8e014ecaf9
Xing <xingmarc@yeah.net>
2018-07-16 23:23:55 -0700

    ..

--- r_u_s_t/hello/src/main.rs
+++ r_u_s_t/hello/src/main.rs
@@ -1,4 +1,8 @@
+use std::io::prelude::*;
+use std::net::TcpStream;
 use std::net::TcpListener;
+
+use std::fs::File;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -6,7 +10,23 @@ fn main() {
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        println!("Connection established!");
+        handle_connection(stream);
+
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+
+    stream.read(&mut buffer).unwrap();
+
+    let mut file = File::open("hello.html").unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+
+    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
 
