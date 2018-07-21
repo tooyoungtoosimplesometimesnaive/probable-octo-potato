3a3597b0bbf91667c48824430068671d92d288e5
Xing <xingmarc@yeah.net>
2018-07-20 22:46:59 -0700

    ...

--- r_u_s_t/hello/src/lib.rs
+++ r_u_s_t/hello/src/lib.rs
@@ -1,6 +1,9 @@
 use std::thread;
 use std::sync::mpsc;
 
+use std::sync::Arc;
+use std::sync::Mutex;
+
 pub struct ThreadPool {
     workers: Vec<Worker>,
     sender: mpsc::Sender<Job>,
@@ -21,10 +24,12 @@ impl ThreadPool {
 
         let (sender, receiver) = mpsc::channel();
 
-        let mut threads = Vec::with_capacity(size);
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        let mut workers = Vec::with_capacity(size);
 
         for id in 0..size {
-            workers.push(Worker::new(id, receiver));
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
 
         ThreadPool {
@@ -46,7 +51,7 @@ struct Worker {
 }
 
 impl Worker {
-    fn new(id: usize, receiver: mpsc::Receiver<Job>) -> Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
         let thread = thread::spawn(|| {
             receiver;
         });
