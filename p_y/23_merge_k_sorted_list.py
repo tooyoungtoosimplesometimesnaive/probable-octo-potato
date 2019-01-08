06c736a07a64572c0fd9d706c9f047f609cddae3
Xing <xingmarc@yeah.net>
2019-01-07 23:59:07 -0800

    ...

--- p_y/23_merge_k_sorted_list.py
+++ p_y/23_merge_k_sorted_list.py
@@ -28,3 +28,32 @@ class Solution(object):
         
         return result.next
 
+
+# Take two:
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
+        if len(lists) == 0:
+            return []
+
+        pq_pool = []
+        for l in lists:
+            if l != None:
+                heapq.heappush(pq_pool, (l.val, l))
+
+        dummy_head = ListNode(0)
+        p = dummy_head
+        while len(pq_pool) > 0:
+            next_node_val, next_node = heapq.heappop(pq_pool)
+            p.next = next_node
+            p = p.next
+
